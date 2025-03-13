d32f5af3ba482f0958d102872191e53f4243f1e8
Mohul Y P <mohulyp@gmail.com>
2025-03-13 20:23:23 +0530

    new

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello 1 2 3\n";
+  cout << "What's up\n";
+  cout << "New C file created by me\n";
+  return 0;
+}

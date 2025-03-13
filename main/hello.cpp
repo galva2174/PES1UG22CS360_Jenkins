93b8d262bb4f39d37ef7bcfce8607ef31dd32028
Mohul Y P <mohulyp@gmail.com>
2025-03-13 16:29:50 +0530

    initial

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello World\n";
+  cout << "Hello World my SRN is PES1UG22CS360\n";
   cout << "Hello Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
